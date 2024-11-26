213d218ef15c1e5bb2a1be2b78e0122ecd4adcbe
Hassam Ahmad <144841227+99Hassam99@users.noreply.github.com>
2024-11-26 18:29:47 +0500

    Add files via upload

--- 09_SQL/02_SELECT_Query_MYSQL.sql
+++ 09_SQL/02_SELECT_Query_MYSQL.sql
@@ -1,3 +1,4 @@
+SELECT * FROM movies;
 SELECT * FROM movies WHERE imdb_rating>=9;
 SELECT * FROM movies WHERE imdb_rating>9;
 SELECT * FROM movies WHERE imdb_rating<7;
@@ -15,4 +16,19 @@ SELECT * FROM movies WHERE imdb_rating is NULL;
 # Not Null
 SELECT * FROM movies WHERE imdb_rating is NOT NULL;
 # print all movies with imdb_rating
-SELECT * FROM movies Where industry='bollywood' ORDER BY imdb_rating
+# By default it is in ascending order
+SELECT * FROM movies Where industry='bollywood' ORDER BY imdb_rating;
+# Descending order
+SELECT * FROM movies Where industry='bollywood' ORDER BY imdb_rating desc;
+
+# By default it is always in ascending order but we can use ascending order command
+
+SELECT * FROM movies Where industry='bollywood' ORDER BY imdb_rating asc;
+
+# To retrieve top rated movies for bollywood
+SELECT * FROM movies Where industry='bollywood' ORDER BY imdb_rating DESC LIMIT 5;
+
+# To retrieve top rated movies for hollywood
+SELECT * FROM movies Where industry='hollywood' ORDER BY imdb_rating DESC LIMIT 5;
+# skipping the 1st index i-e 0 index
+SELECT * FROM movies Where industry='hollywood' ORDER BY imdb_rating DESC LIMIT 5 OFFSET 1;
